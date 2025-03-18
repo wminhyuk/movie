48a69d48c1d2c0c7b928f9d8f21892ed18392f31
wminhyuk <seoseo1998@gmail.com>
2025-03-18 09:50:04 +0900

    clone

--- src/movie/api/call.py
+++ src/movie/api/call.py
@@ -3,10 +3,10 @@ import requests
 import pandas as pd
 
 BASE_URL = "http://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json"
-KEY=os.getenv('MOVIE_KEY')
+KEY=os.getenv("MOVIE_KEY")
 
 def gen_url(dt="20120101", url_param={}):
-    "호출 URL 생성, url_param이 입력되면 multiMovieYn, repNationCd 처리"
+    "호출 URL 생성, url_param 이 입력되면 multiMovieYn, repNationCd 처리"
     url = f"{BASE_URL}?key={KEY}&targetDt={dt}"
     
     # TODO = url_param 처리
@@ -21,13 +21,23 @@ def call_api(dt="20120101", url_param={}):
     j = data.json()
     return j['boxOfficeResult']['dailyBoxOfficeList']
 
-
-def list2df(data: list, dt: str):
+def list2df(data: list, dt: str, url_params={}):
     df = pd.DataFrame(data)
-    df["dt"] = dt
+    df['dt'] = dt
+    for k, v in url_params.items():
+        df[k] = v
+    
+    num_cols = ['rnum', 'rank', 'rankInten', 'salesAmt', 'audiCnt',
+                'audiAcc', 'scrnCnt', 'showCnt', 'salesShare', 'salesInten',
+                'salesChange', 'audiInten', 'audiChange']
+    df[num_cols] = df[num_cols].apply(pd.to_numeric) 
+
     return df
 
-def save_df(df, base_path):
-    df.to_parquet(base_path, partition_cols=['dt'])
-    save_path = f"{base_path}/dt={df['dt'][0]}"
+def save_df(df, base_path, partitions=['dt']):
+    df.to_parquet(base_path, partition_cols=partitions)
+    save_path = base_path
+    for p in partitions:
+        save_path = save_path + f"/{p}={df[p][0]}"
     return save_path
+
--- tests/test_call.py
+++ tests/test_call.py
@@ -5,24 +5,23 @@ import pandas as pd
 def test_gen_url_default():
     r = gen_url()
     print(r)
-    assert "kovis" in r
+    assert "kobis" in r
     assert "targetDt" in r
-    assert os.getenv('MOVIE_KEY') in r
-    
+    assert os.getenv("MOVIE_KEY") in r
 
-def test_gen_url_default():
-    r = gen_url(url_param={'multiMovieYn':'Y', 'repNationCd':'K'})
+def test_gen_url():
+    r = gen_url(url_param={"multiMovieYn": "Y", "repNationCd": "K"})
     assert "&multiMovieYn=Y" in r
-    assert "&repNationCd" in r
+    assert "&repNationCd=K" in r
 
 def test_call_api():
-    r=call_api()
+    r = call_api()
     assert isinstance(r, list)
-    assert isinstance(r[0]['rnum'],str)
+    assert isinstance(r[0]['rnum'], str)
     assert len(r) == 10
     for e in r:
         assert isinstance(e, dict)
-        
+
 def test_list2df():
     ymd = "20210101"
     data = call_api(dt=ymd)
@@ -31,8 +30,8 @@ def test_list2df():
     assert len(data) == len(df)
     assert set(data[0].keys()).issubset(set(df.columns))
     assert "dt" in df.columns, "df 컬럼이 있어야 함"
-    assert (df["dt"] == ymd).all(), "모든 컬럼에 입력된 날짜 값이 존재 해야 함"
-    
+    assert (df["dt"] == ymd).all(), "입력된 날짜 값이 컬럼  존재 해야 함"
+
 def test_save_df():
     ymd = "20210101"
     data = call_api(dt=ymd)
@@ -44,3 +43,38 @@ def test_save_df():
     read_df = pd.read_parquet(r)
     assert 'dt' not in read_df.columns
     assert 'dt' in pd.read_parquet(base_path).columns
+
+
+def test_save_df_url_params():
+    ymd = "20210101"
+    url_params = {"multiMovieYn": "Y"}
+    base_path = "~/temp/movie"
+    
+    # TO Airflow Dag
+    data = call_api(dt=ymd, url_param=url_params)
+    df = list2df(data, ymd, url_params)
+    partitions = ['dt'] + list(url_params.keys())
+    r = save_df(df, base_path, partitions)
+    
+    assert r == f"{base_path}/dt={ymd}/multiMovieYn=Y"
+    print("save_path", r)
+    read_df = pd.read_parquet(r)
+    assert 'dt' not in read_df.columns
+    assert 'dt' in pd.read_parquet(base_path).columns
+
+def test_list2df_check_num():
+    """df 에 숫자 컬럼을 변환 하고 잘 변환 되었는나 확인"""
+    num_cols = ['rnum', 'rank', 'rankInten', 'salesAmt', 'audiCnt',
+                'audiAcc', 'scrnCnt', 'showCnt', 'salesShare', 'salesInten',
+                'salesChange', 'audiInten', 'audiChange']
+    
+    # hint : 변환 : df[num_cols].apply(pd.to_numeric)
+    # hint : 확인 : is_numeric_dtype <- pandas ...
+    ymd = "20210101"
+    data = call_api(dt=ymd)
+    df = list2df(data, ymd)
+    for c in num_cols:
+        assert df[c].dtype in ['int64', 'float64'], f"{c} 가 숫자가 아님"
+
+
+
